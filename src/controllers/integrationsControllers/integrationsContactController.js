ee0432389ace8d67a8bb9eceeb9149bcedffc500
Johan Doncel <donceljohan0@gmail.com>
2024-03-04 10:28:52 -0500

    testing fix input object in request

--- src/controllers/integrationsControllers/integrationsContactController.js
+++ src/controllers/integrationsControllers/integrationsContactController.js
@@ -4,31 +4,33 @@ const hubspotClient = new hubspot.Client({ accessToken: "pat-na1-7cb6c5c1-e502-4
 
 const createOrUpdateContact = async (req, res) => {
 	try {
-		console.log(req.body);
-		/* await hubspotClient.crm.contacts.basicApi.getById(req.body.id)
+		console.log(req.body.vid);
+		//await hubspotClient.crm.contacts.basicApi.getById(req.body.vid)
 		const properties = {
 			properties: {
-				character_id: req.body.properties.character_id,
-				firstname: req.body.properties.firstname,
-				lastname: req.body.properties.lastname,
-				status_character: req.body.properties.status_character,
-				character_species: req.body.properties.character_species,
-				character_gender: req.body.properties.character_gender
+				character_id: req.body.properties.character_id.value,
+				firstname: req.body.properties.firstname.value,
+				lastname: req.body.properties.lastname.value,
+				status_character: req.body.properties.status_character.value,
+				character_species: req.body.properties.character_species.value,
+				character_gender: req.body.properties.character_gender.value
 			}
 		}
-		const updateContactResponse = await hubspotClient.crm.contacts.basicApi.update(req.body.id, properties);
-		console.log(updateContactResponse);
-		res.json({ success: true, message: 'Contact updated successfully' }); */
+
+		console.log(properties);
+		/* const updateContactResponse = await hubspotClient.crm.contacts.basicApi.update(req.body.vid, properties);
+		console.log(updateContactResponse); */
+		res.json({ success: true, message: 'Contact updated successfully' });
 	} catch (error) {
 		if (error && error.code === 404) {
 			const properties = {
 				properties: {
-					character_id: req.body.properties.character_id,
-					firstname: req.body.properties.firstname,
-					lastname: req.body.properties.lastname,
-					status_character: req.body.properties.status_character,
-					character_species: req.body.properties.character_species,
-					character_gender: req.body.properties.character_gender
+					character_id: req.body.properties.character_id.value,
+					firstname: req.body.properties.firstname.value,
+					lastname: req.body.properties.lastname.value,
+					status_character: req.body.properties.status_character.value,
+					character_species: req.body.properties.character_species.value,
+					character_gender: req.body.properties.character_gender.value
 				}
 			}
 			const createContactResponse = await hubspotClient.crm.contacts.basicApi.create(properties);
