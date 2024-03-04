39d0356b38b9f104f0a7d883924ffafc098c79e9
Johan Doncel <donceljohan0@gmail.com>
2024-03-04 13:01:27 -0500

    Refactor methods createOrUptade of the contacts and companies

--- src/controllers/integrationsControllers/integrationsCompanyController.js
+++ src/controllers/integrationsControllers/integrationsCompanyController.js
@@ -3,40 +3,52 @@ const { formatDate } = require('../../utils/utils')
 
 const hubspotClient = new hubspot.Client({ accessToken: "pat-na1-7cb6c5c1-e502-4b5e-84b4-826a28dca2db" });
 
+
+const createSearchCompany = (value) => {
+	return searchContact = {
+		filterGroups: [{
+			filters: [{
+				propertyName: "location_id",
+				operator: "EQ",
+				value: value
+			}]
+		}]
+	}
+}
+
+const createPropertiesCompany = (bodyProperties) => {
+	return properties = {
+		properties: {
+			location_id: bodyProperties.location_id.value,
+			name: bodyProperties.name.value,
+			location_type: bodyProperties.location_type.value,
+			dimension: bodyProperties.dimension.value,
+			creation_date: formatDate(bodyProperties.creation_date.value)
+		}
+	}
+}
+
 const createOrUpdateCompany = async (req, res) => {
 	try {
-		const responseCompany = await hubspotClient.crm.companies.basicApi.getById(req.body.vid);
+		const searchCompany = createSearchCompany(req.body.properties.location_id.value)
+		const properties = createPropertiesCompany(req.body.properties);
+
+		const responseCompany = await hubspotClient.crm.contacts.searchApi.doSearch(searchCompany);
 		console.log(responseCompany);
-		const properties = {
-			properties: {
-				location_id: req.body.properties.location_id.value,
-				name: req.body.properties.name.value,
-				location_type: req.body.properties.location_type.value,
-				dimension: req.body.properties.dimension.value,
-				creation_date: formatDate(req.body.properties.creation_date.value)
-			}
-		}
-		const updateContactResponse = await hubspotClient.crm.companies.basicApi.update(req.body.vid, properties);
-		console.log(updateContactResponse);
-		res.json({ success: true, message: 'Company updated successfully' });
-	} catch (error) {
-		if (error && error.code === 404) {
-			const properties = {
-				properties: {
-					location_id: req.body.properties.location_id.value,
-					name: req.body.properties.name.value,
-					location_type: req.body.properties.location_type.value,
-					dimension: req.body.properties.dimension.value,
-					creation_date: formatDate(req.body.properties.creation_date.value)
-				}
-			}
+
+		if (responseCompany.results.length > 0) {
+			const updateContactResponse = await hubspotClient.crm.companies.basicApi.update(req.body.vid, properties);
+			console.log(updateContactResponse);
+			res.json({ success: true, message: 'Company updated successfully' });
+		} else {
 			const createContactResponse = await hubspotClient.crm.companies.basicApi.create(properties);
 			console.log(createContactResponse);
 			res.json({ success: true, message: 'Company created successfully' });
-		} else {
-			console.error(error);
-			res.status(500).json({ success: false, message: "Internal Server Error" })
 		}
+
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ success: false, message: "Internal Server Error" })
 	}
 };
 
--- src/controllers/integrationsControllers/integrationsContactController.js
+++ src/controllers/integrationsControllers/integrationsContactController.js
@@ -2,42 +2,53 @@ const hubspot = require('@hubspot/api-client')
 
 const hubspotClient = new hubspot.Client({ accessToken: "pat-na1-7cb6c5c1-e502-4b5e-84b4-826a28dca2db" });
 
+
+const createSearchContact = (value) => {
+	return searchContact = {
+		filterGroups: [{
+			filters: [{
+				propertyName: "character_id",
+				operator: "EQ",
+				value: value
+			}]
+		}]
+	}
+}
+
+const createPropertiesContact = (bodyProperties) => {
+	return properties = {
+		properties: {
+			character_id: bodyProperties.character_id.value,
+			firstname: bodyProperties.firstname.value,
+			lastname: bodyProperties.lastname.value,
+			status_character: bodyProperties.status_character.value,
+			character_species: bodyProperties.character_species.value,
+			character_gender: bodyProperties.character_gender.value
+		}
+	}
+}
+
 const createOrUpdateContact = async (req, res) => {
 	try {
-		const responseContact = await hubspotClient.crm.contacts.basicApi.getById(req.body.vid)
+		const searchContact = createSearchContact(req.body.properties.character_id.value);
+		const properties = createPropertiesContact(req.body.properties);
+		
+		const responseContact = await hubspotClient.crm.contacts.searchApi.doSearch(searchContact)
 		console.log(responseContact);
-		const properties = {
-			properties: {
-				character_id: req.body.properties.character_id.value,
-				firstname: req.body.properties.firstname.value,
-				lastname: req.body.properties.lastname.value,
-				status_character: req.body.properties.status_character.value,
-				character_species: req.body.properties.character_species.value,
-				character_gender: req.body.properties.character_gender.value
-			}
-		};
-		const updateContactResponse = await hubspotClient.crm.contacts.basicApi.update(req.body.vid, properties);
-		console.log(updateContactResponse);
-		res.json({ success: true, message: 'Contact updated successfully' });
-	} catch (error) {
-		if (error && error.code === 404) {
-			const properties = {
-				properties: {
-					character_id: req.body.properties.character_id.value,
-					firstname: req.body.properties.firstname.value,
-					lastname: req.body.properties.lastname.value,
-					status_character: req.body.properties.status_character.value,
-					character_species: req.body.properties.character_species.value,
-					character_gender: req.body.properties.character_gender.value
-				}
-			}
+
+		if (responseContact.results.length > 0) {
+			const updateContactResponse = await hubspotClient.crm.contacts.basicApi.update(req.body.vid, properties);
+			console.log(updateContactResponse);
+			res.json({ success: true, message: 'Contact updated successfully' });
+		} else {
 			const createContactResponse = await hubspotClient.crm.contacts.basicApi.create(properties);
 			console.log(createContactResponse);
 			res.json({ success: true, message: 'Contact created successfully' });
-		} else {
-			console.error(error);
-			res.status(500).json({ success: false, message: "Internal Server Error" })
 		}
+
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ success: false, message: "Internal Server Error" })
 	}
 };
 
